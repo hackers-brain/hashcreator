e1796c1d3e5cf9a9622b20214ac37d46afda6842
Gaurav Raj <36809025+hackers-brain@users.noreply.github.com>
2020-05-09 16:04:57 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+try:
+    os.system("python3 install python3-colorama")
+    os.system("pip3 install colorama")
+    os.system("clear")
+    print(" Setup Installed Successfully !!!")
+    os.system("python3 hashcreator.py")
+except Exception as error:
+    print(f"\n {error}\n  Error Encountered...")
